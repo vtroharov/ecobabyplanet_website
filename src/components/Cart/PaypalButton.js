b333d06f530c22a08ec1c52bfe878698a2acf3c1
vtroharov <vtroharov@users.noreply.github.com>
2020-05-31 17:03:30 +1000

    Paypal Fix #2

--- src/components/Cart/PaypalButton.js
+++ src/components/Cart/PaypalButton.js
@@ -24,14 +24,14 @@ export default class MyApp extends React.Component {
             // => sometimes it may take about 0.5 second for everything to get set, or for the button to appear
         }
  
-        let env = 'production'; // you can set here to 'production' for production
+        let env = 'sandbox'; // you can set here to 'production' for production
         let currency = 'AUD'; // or you can set this value from your props or state
         let total = this.props.totalAmount; // same as above, this is the total amount (based on currency) to be paid by using Paypal express checkout
         // Document on Paypal's currency code: https://developer.paypal.com/docs/classic/api/currency_codes/
  
         const client = {
-            //sandbox: process.env.REACT_APP_APPL_ID,   
-            production: process.env.REACT_APP_APPL_ID,
+            sandbox: process.env.REACT_APP_SAND_ID,   
+            production: process.env.REACT_APP_PROD_ID,
         }
         // In order to get production's app-ID, you will have to send your app to Paypal for approval first
         // For sandbox app-ID (after logging into your developer account, please locate the "REST API apps" section, click "Create App"):
